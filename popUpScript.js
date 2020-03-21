9722956b138e6e4985718e90081350b3fe2f2eac
Sarah Hulton <sphulton@gmail.com>
2020-03-21 15:52:45 -0400

    Update popUpScript.js

--- popUpScript.js
+++ popUpScript.js
@@ -1,23 +1,28 @@
 
 //SHOWS THE POPUP WITH THE RESPONSE CORRESPODNING TO THAT ANIMATION
-function showPopup(answer, styleId){
+function showPopup(styleId, timeStamp, answer){
   //error message for no answer
   if (answer==undefined)
   {
     answer="no answer defined";
   }
 
-  if (styleId=='action'){
-    document.getElementById('popUpBox').style.borderColor='#acd5ed';
+  //changing colors to match category
+  if (styleId=='concept'){
+    document.getElementById('popUpBox').style.borderColor='#64a9bc';
+    document.getElementById('timeStamp').style.color='#64a9bc';
   }
-  else if (styleId=='ship'){
-    document.getElementById('popUpBox').style.borderColor='tomato';
+  else if (styleId=='action'){
+    document.getElementById('popUpBox').style.borderColor='#e8e574';
+    document.getElementById('timeStamp').style.color='#e8e574';
   }
   else if (styleId=='item'){
-    document.getElementById('popUpBox').style.borderColor='#e3b726';
+    document.getElementById('popUpBox').style.borderColor='#7b964d';
+    document.getElementById('timeStamp').color='#7b964d';
   }
-  else if (styleId=='concept'){
-    document.getElementById('popUpBox').style.borderColor='#bb91b6';
+  else if (styleId=='relationship'){
+    document.getElementById('popUpBox').style.borderColor='#ed7666';
+    document.getElementById('timeStamp').style.color='#ed7666';
   }
   else {
     //adding black to debug
@@ -27,8 +32,10 @@ function showPopup(answer, styleId){
   //MAKING THE POPUP AND DIMMED SCREEN VISIBLE
   document.getElementById('popUpBox').style.visibility='visible';
   document.getElementById('dimmedScreen').style.visibility='visible';
-  //printing text
+
+  //printing answer/timestamp
   document.getElementById("answerText").innerHTML = answer;
+  document.getElementById("timeStamp").innerHTML = '- ' + timeStamp;
 }
 
 //CLOSES THE POPUP
